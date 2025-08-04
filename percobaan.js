9de8bfd047df0e77ada064e796d4419bd118ede4
Spacastre <spacastreteam@gmail.com>
2025-08-04 16:27:20 +0700

    membuat carousel index

---
+++ percobaan.js
@@ -0,0 +1,35 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // Autoplay configuration
+  autoplay: {
+    delay: 3000, // 3 detik
+    disableOnInteraction: false, // tetap lanjut setelah interaksi user
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },        
+  }
+});
